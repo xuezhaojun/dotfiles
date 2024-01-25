581efe3beff9f9313259b09e79451f9cf596b8e3
xuezhao <zxue@redhat.com>
2024-01-25 13:16:56 +0000

    Install brew upfront; Create symlink for config.fish
    Signed-off-by: GitHub <noreply@github.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+# create a function to symklink files to the home directory
+createsymlink() {
+    echo "Creating symlink to $1"
+    ln -sf $PWD/$1 $HOME//$1
+}
+
+# -- install fish shell related plugins
+
+echo "Creat symlink to config.fish"
+createsymlink .config/fish/config.fish # env variables are set in config.fish
+
+echo "Install brew"
+echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# -- install stolostron related dependencies
+
 echo "Install go libs"
-sh ./install/go.sh
-sh ./install/quay.sh
+sh ./deps/go.sh
+
+echo "Login to quay.io"
+sh ./deps/quay.sh
