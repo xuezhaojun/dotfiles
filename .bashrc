afb34e03874005c59a8115a99e82a866ffefe021
xuezhao <zxue@redhat.com>
2024-01-25 13:58:10 +0000

    Using bash instead of fish.
    Signed-off-by: GitHub <noreply@github.com>

--- .bashrc
+++ .bashrc
@@ -1,3 +1,3 @@
 # To fix error when run make in multicluster-operators-foundation repo:
 # vendor/github.com/openshift/build-machinery-go/make/targets/golang/../../lib/golang.mk:23: *** `go` is required with minimal version "1.15.2", detected version "1.21.6". You can override this check by using `make GO_REQUIRED_MIN_VERSION:=`.  Stop.
-set -x GO_REQUIRED_MIN_VERSION ""
+export GO_REQUIRED_MIN_VERSION=""
--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+
 # create a function to symklink files to the home directory
 createsymlink() {
     echo "Creating symlink to $1"
     ln -sf $PWD/$1 $HOME//$1
 }
 
-# -- install fish shell related plugins
-
-echo "Creat symlink to config.fish"
-createsymlink .config/fish/config.fish # env variables are set in config.fish
+echo "Creat symlinks"
+createsymlink .bashrc
+createsymlink .config/starship.toml
 
 echo "Install brew"
 echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/codespace/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.bashrc
+
+echo "Install startship"
+brew install starship
 
 # -- install stolostron related dependencies
 
