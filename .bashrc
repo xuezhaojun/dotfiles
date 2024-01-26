f786ca4292212357185631e00c304e45bd32cbf3
xuezhao <zxue@redhat.com>
2024-01-26 16:33:33 +0000

    Usng zshrc.
    Signed-off-by: GitHub <noreply@github.com>

--- .bashrc
+++
@@ -1,3 +0,0 @@
-# To fix error when run make in multicluster-operators-foundation repo:
-# vendor/github.com/openshift/build-machinery-go/make/targets/golang/../../lib/golang.mk:23: *** `go` is required with minimal version "1.15.2", detected version "1.21.6". You can override this check by using `make GO_REQUIRED_MIN_VERSION:=`.  Stop.
-export GO_REQUIRED_MIN_VERSION=""
--- install.sh
+++ install.sh
@@ -6,24 +6,23 @@ createsymlink() {
     ln -sf $PWD/$1 $HOME//$1
 }
 
-echo "Creat symlinks"
-createsymlink .bashrc
-createsymlink .config/starship.toml
-
 echo "Install brew"
 echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/codespace/.bashrc
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/codespace/.zshrc
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-source ~/.bashrc
 
-echo "Install startship"
-brew install starship
-(echo; echo 'eval "$(starship init bash)"') >> /home/codespace/.bashrc
+brew install autojump
+brew install zsh-autosuggestions
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /home/codespace/.zshrc
+echo "[ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh" >> /home/codespace/.zshrc
 
 # -- install stolostron related dependencies
-
 echo "Install go libs"
 sh ./deps/go.sh
 
 echo "Login to quay.io"
 sh ./deps/quay.sh
+
+# To fix error when run make in multicluster-operators-foundation repo:
+# vendor/github.com/openshift/build-machinery-go/make/targets/golang/../../lib/golang.mk:23: *** `go` is required with minimal version "1.15.2", detected version "1.21.6". You can override this check by using `make GO_REQUIRED_MIN_VERSION:=`.  Stop.
+echo "export GO_REQUIRED_MIN_VERSION=\"\"" >> /home/codespace/.zshrc
