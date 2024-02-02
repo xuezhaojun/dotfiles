96a88d97a317ca866acb6bb68e4a8145d2829cb7
xuezhao <zxue@redhat.com>
2024-02-02 10:33:22 +0000

    Change order.
    Signed-off-by: GitHub <noreply@github.com>

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ sh ./go.sh
 echo "Restore the docker auths"
 sh ./auth_merge.sh
 
+echo "Set special environment variables"
+# To fix error when run make in multicluster-operators-foundation repo:
+# vendor/github.com/openshift/build-machinery-go/make/targets/golang/../../lib/golang.mk:23: *** `go` is required with minimal version "1.15.2", detected version "1.21.6". You can override this check by using `make GO_REQUIRED_MIN_VERSION:=`.  Stop.
+echo "export GO_REQUIRED_MIN_VERSION=\"\"" >> /home/codespace/.zshrc
+
 echo "Install brew and some brew packages"
 echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/codespace/.zshrc
@@ -16,7 +21,3 @@ brew install autojump
 brew install zsh-autosuggestions
 echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /home/codespace/.zshrc
 echo "[ -f /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh ] && . /home/linuxbrew/.linuxbrew/etc/profile.d/autojump.sh" >> /home/codespace/.zshrc
-
-# To fix error when run make in multicluster-operators-foundation repo:
-# vendor/github.com/openshift/build-machinery-go/make/targets/golang/../../lib/golang.mk:23: *** `go` is required with minimal version "1.15.2", detected version "1.21.6". You can override this check by using `make GO_REQUIRED_MIN_VERSION:=`.  Stop.
-echo "export GO_REQUIRED_MIN_VERSION=\"\"" >> /home/codespace/.zshrc
